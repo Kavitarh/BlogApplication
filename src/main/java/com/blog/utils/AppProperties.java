0f94e910f7144ffceecd857b59f30abba1d455f6
Kavita Honnalli <kavita.rh92@gmail.com>
2026-03-01 13:17:59 +0530

    Add files via upload

---
+++ src/main/java/com/blog/utils/AppProperties.java
@@ -0,0 +1,9 @@
+package com.blog.utils;
+
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="dynjh1lwj";
+   public static final String CLOUDINARY_API_KEY="383222833357662";
+   public static final String CLOUDINARY_SECRET_KEY="hFSFoJY9adSFCZ3VOH0Cc5DzBRg";
+}
